a87f336ffc071258ea71e56d677ad1aee0562758
AzureAD\PramodKumar <pramod.kumar@ddsplm.com>
2026-02-06 14:20:59 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+// const cors = require('cors');
+// require('dotenv').config();
+// const multer = require('multer');
+// const path = require('path');
+
+// // Multer storage configuration
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     }
+// });
+
+// const upload = multer({ storage: storage });
+
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+// Ensure uploads folder exists
+const uploadDir = path.join(__dirname, 'uploads');
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
 
 // Multer storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 const app = express();
 app.use(cors());
